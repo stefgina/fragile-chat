df6af4ff942e1ff7767f9ea3f614ee46b669f739
stefgina <stefginaedu@gmail.com>
2022-06-14 01:46:56 +0300

    frontend-login-create-pages

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000')
+const socket = io('http://192.168.0.180:3000')
 const messageContainer = document.getElementById('message-container')
 const roomContainer = document.getElementById('room-container')
 const messageForm = document.getElementById('send-container')
